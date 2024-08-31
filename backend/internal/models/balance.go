f95f1344182a61e5e904be00e053906574c5f59c
Arizidane <samionohari@gmail.com>
2024-08-31 22:19:55 +0700

    fix: balance model

--- backend/internal/models/balance.go
+++ backend/internal/models/balance.go
@@ -6,7 +6,7 @@ type Balance struct {
 	ID             uint      `json:"id" gorm:"primaryKey"`
 	UserID         uint      `json:"user_id" gorm:"not null"`
 	CurrentBalance float64   `json:"current_balance" gorm:"not null;default:0"`
-	Currency       string    `json:"currency" gorm:"type:varchar(3);not null;default:'USD'"`
+	Currency       string    `json:"currency" gorm:"type:varchar(5);not null;default:'USD'"`
 	LastUpdated    time.Time `json:"last_updated" gorm:"type:timestamp;not null;default:CURRENT_TIMESTAMP"`
 	User           User      `json:"user" gorm:"foreignKey:UserID"`
 }

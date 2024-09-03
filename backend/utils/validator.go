6cfa42ef54260546e7e46b17eb564b0d931a7551
Arizidane <samionohari@gmail.com>
2024-09-03 23:09:27 +0700

    feat: user ID from Context

--- backend/utils/validator.go
+++ backend/utils/validator.go
@@ -1,7 +1,11 @@
 package utils
 
 import (
+	"errors"
+
+	"github.com/arizdn234/EvoPay/internal/models"
 	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
 )
 
 var validate = validator.New()
@@ -9,4 +13,12 @@ var validate = validator.New()
 // ValidateStruct validates the given struct based on tags.
 func ValidateStruct(s interface{}) error {
 	return validate.Struct(s)
+}
+
+func GetUserIDFromContext(c *fiber.Ctx) (uint64, error) {
+	user, exists := c.Locals("user").(*models.User) // Adjust based on how you store the user info in context
+	if !exists {
+		return 0, errors.New("user not found in context")
+	}
+	return uint64(user.ID), nil // Assuming `ID` is of type uint64
 }

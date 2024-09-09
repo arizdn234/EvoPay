445e7a9ac0472e0a3e00c516cc78b728eb84e9c8
Arizidane <samionohari@gmail.com>
2024-09-09 20:37:22 +0700

    fix: main

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,49 @@
 package main
 
 import (
+	"log"
+	"os"
+
+	"github.com/arizdn234/EvoPay/internal/config"
+	"github.com/arizdn234/EvoPay/internal/db"
+	"github.com/arizdn234/EvoPay/internal/redis"
+	"github.com/arizdn234/EvoPay/internal/repository"
+	"github.com/arizdn234/EvoPay/internal/server"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	// Load environment variables
+	config.LoadEnv()
+
+	// Initialize Redis
+	redis.Initialize()
+
+	// Initialize the database
+	db.InitDB()
+
+	// Migrate
+	if err := repository.Migrate(db.DB); err != nil {
+		log.Fatalf("Failed to migrate the table: %v", err)
+	}
+
+	// Seed data
+	if err := repository.NewUserRepository(db.DB).Seed(); err != nil {
+		log.Fatalf("Failed to seed data: %v", err)
+	}
+
+	// Initialize Fiber app
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	// Get the port from the environment variables
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-	app.Listen(":8080")
+	server.RunServer(app, db.DB, port)
+	log.Printf("Server is running on port %v\n\nhttp://localhost:%v", port, port)
+	if err := app.Listen(":" + port); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }

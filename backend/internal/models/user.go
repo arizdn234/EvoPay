5e3e50e52e969057e81ada90174ae6e3bba77750
Arizidane <samionohari@gmail.com>
2024-09-09 20:41:46 +0700

    feta: User Model: define and manage user models and validation

---
+++ backend/internal/models/user.go
@@ -0,0 +1,44 @@
+package models
+
+import "time"
+
+type Role struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Name      string    `json:"name" gorm:"type:varchar(255);unique;not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Permission struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Name      string    `json:"name" gorm:"type:varchar(255);unique;not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type RolePermission struct {
+	RoleID       uint `json:"role_id" gorm:"primaryKey"`
+	PermissionID uint `json:"permission_id" gorm:"primaryKey"`
+}
+
+type User struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Name      string    `json:"name" gorm:"type:varchar(255);"`
+	Email     string    `json:"email" gorm:"type:varchar(255);not null"`
+	Password  string    `json:"password" gorm:"type:varchar(255);not null"`
+	RoleID    uint      `json:"role_id" gorm:"not null"`
+	Role      Role      `json:"role" gorm:"foreignKey:RoleID"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type UserLogin struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type UserRegister struct {
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}

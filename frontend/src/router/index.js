b89874a8026432bbe8af290e97e09d30453f8551
Arizidane <samionohari@gmail.com>
2024-09-10 22:45:52 +0700

    feat: welcome view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,18 +4,20 @@ import HomeView from '../views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: () => import('../views/AboutView.vue')
+    // },
     {
       path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      name: 'documentation',
+      component: () => import('../views/DocumentationView.vue')
     }
   ]
 })
---
+++ frontend/src/views/DocumentationView.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="documentation">
+      <h1>{{ info.service }} API Documentation</h1>
+      <p>Version: {{ info.version }}</p>
+      <div class="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Method</th>
+              <th>Path</th>
+              <th>Description</th>
+              <th>Note</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(route, index) in info.routes" :key="index">
+              <td>{{ route.method }}</td>
+              <td>{{ route.path }}</td>
+              <td>{{ route.description }}</td>
+              <td v-if="route.note">{{ route.note }}</td>
+              <td v-else>-</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <p>*{{ info.note }}</p>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue'
+  import axios from 'axios'
+  
+  const info = ref({})
+  
+  onMounted(async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:8080/')  // Ganti dengan URL endpoint backend Anda
+      info.value = response.data
+    } catch (error) {
+      console.error('Error fetching documentation:', error)
+    }
+  })
+  </script>
+  
+  <style scoped>
+  .documentation {
+    padding: 20px;
+    max-width: 1000px;
+    margin: 0 auto;
+  }
+  
+  .documentation h1 {
+    font-size: 2em;
+    margin-bottom: 20px;
+  }
+  
+  .table-container {
+    overflow-x: auto; /* Enable horizontal scrolling */
+    width: 100%; /* Ensure container takes full width */
+  }
+  
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 20px;
+  }
+  
+  th, td {
+    border: 1px solid #dddddd;
+    padding: 8px;
+    text-align: left;
+  }
+  
+  th {
+    background-color: #1a282e;
+    font-weight: bold;
+    color: #ffffff; /* Text color for better contrast */
+  }
+  
+  tr {
+    transition: all .5s;
+  }
+  
+  tr:nth-child(even) {
+    background-color: #303030;
+  }
+  
+  tr:hover {
+    background-color: #444444;
+  }
+  
+  td {
+    white-space: nowrap; /* Prevent text from wrapping */
+  }
+  
+  @media (max-width: 600px) {
+    .documentation h1 {
+      font-size: 1.5em;
+    }
+  }
+  </style>
+  

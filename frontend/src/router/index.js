ab57cdea5d88d4cb4fa8b3371aab77daac33ef8c
Arizidane <samionohari@gmail.com>
2024-09-11 15:36:27 +0700

    fix: view routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DocumentationView from '@/views/DocumentationView.vue'
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
+import LogoutView from '@/views/LogoutView.vue'
+import VerifyEmailView from '@/views/VerifyEmailView.vue'
+import ResetPasswordView from '@/views/ResetPasswordView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,7 +23,32 @@ const router = createRouter({
     {
       path: '/',
       name: 'documentation',
-      component: () => import('../views/DocumentationView.vue')
+      component: DocumentationView
+    },
+    {
+      path: '/users/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/users/register',
+      name: 'register',
+      component: RegisterView
+    },
+    {
+      path: '/users/logout',
+      name: 'logout',
+      component: LogoutView
+    },
+    {
+      path: '/users/verify-email',
+      name: 'verify-email',
+      component: VerifyEmailView
+    },
+    {
+      path: '/users/reset-password',
+      name: 'reset-password',
+      component: ResetPasswordView
     }
   ]
 })

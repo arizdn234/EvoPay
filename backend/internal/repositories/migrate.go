02bec6db5f546f6b76faabe433abc252e19715ab
Arizidane <samionohari@gmail.com>
2024-09-20 16:55:25 +0700

    fix: migration tables function

--- backend/internal/repositories/migrate.go
+++ backend/internal/repositories/migrate.go
@@ -16,18 +16,28 @@ func Migrate(db *gorm.DB) error {
 		return err
 	}
 
-	// Migrate Permission table
-	if err := db.AutoMigrate(&models.Permission{}); err != nil {
-		return err
-	}
-
-	// Migrate RolePermission table
-	if err := db.AutoMigrate(&models.RolePermission{}); err != nil {
-		return err
-	}
-
 	// Migrate Transaction table
 	if err := db.AutoMigrate(&models.Transaction{}); err != nil {
+		return err
+	}
+
+	// Migrate Balance table
+	if err := db.AutoMigrate(&models.Balance{}); err != nil {
+		return err
+	}
+
+	// Migrate Deposit table
+	if err := db.AutoMigrate(&models.Deposit{}); err != nil {
+		return err
+	}
+
+	// Migrate Withdrawal table
+	if err := db.AutoMigrate(&models.Withdrawal{}); err != nil {
+		return err
+	}
+
+	// Migrate TransactionLog table
+	if err := db.AutoMigrate(&models.TransactionLog{}); err != nil {
 		return err
 	}
 

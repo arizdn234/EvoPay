a22b334579e9d3bd857e3dd09fb01b540fcb06cd
Arizidane <samionohari@gmail.com>
2024-10-03 17:55:21 +0700

    feat: form & button component

---
+++ frontend/src/components/CenteredForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex items-center justify-center min-h-screen bg-background-soft">
+        <div class="bg-background rounded-lg shadow-lg p-8 max-w-md w-full">
+            <h1 class="text-2xl font-semibold text-heading text-center mb-6">{{ title }}</h1>
+            <p class="text-center text-text-secondary-light">{{ description }}</p>
+
+            <!-- Form Placeholder -->
+            <div class="space-y-4">
+                <!-- Placeholder for form inputs -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CenteredForm',
+    props: {
+        title: {
+            type: String,
+            default: 'Form Title',
+        },
+        description: {
+            type: String,
+            default: 'Form Description',
+        }
+    },
+};
+</script>
+
+<style scoped></style>
---
+++ frontend/src/components/FeatureButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <button
+      @click="onClick"
+      class="bg-blue-500 text-white font-sfpro font-bold py-2 px-4 rounded hover:bg-blue-700 hover:py-4 transition-all duration-300 ease-in-out"
+    >
+      {{ text }}
+    </button>
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+    text: String,
+    onClick: Function
+  });
+  </script>
+  

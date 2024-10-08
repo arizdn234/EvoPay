dd8211c3fdd7a848207dac585e4f4d270e54a665
Arizidane <samionohari@gmail.com>
2024-10-08 18:37:50 +0700

    feat: dateUtils init

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,19 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { diffForHuman, formatDate } from './dateUtils';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.mixin({
+    methods: {
+        diffForHuman,
+        formatDate
+    }
+});
 
-app.mount('#app')
+app.use(router);
+
+app.mount('#app');

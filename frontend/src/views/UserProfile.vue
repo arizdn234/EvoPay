abf1cab571012a6284c44a503f7115d2f60c4310
Arizidane <samionohari@gmail.com>
2024-10-08 18:38:43 +0700

    fix: fetch transaction & user data

--- frontend/src/views/UserProfile.vue
+++ frontend/src/views/UserProfile.vue
@@ -6,23 +6,29 @@
         <div v-if="userData" class="profile-info bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md">
             <h2 class="text-lg font-semibold mb-4">Profile Information</h2>
             <div class="mb-3">
-                <strong class="text-blue-light dark:text-blue-dark">Name: </strong> <span class="text-grey-light dark:text-white">{{ userData.name }}</span>
+                <strong class="text-blue-light dark:text-blue-dark">Name: </strong> <span
+                    class="text-grey-light dark:text-white">{{ userData.name }}</span>
             </div>
             <div class="mb-3">
-                <strong class="text-blue-light dark:text-blue-dark">Email: </strong> <span class="text-grey-light dark:text-white">{{ userData.email }}</span>
+                <strong class="text-blue-light dark:text-blue-dark">Email: </strong> <span
+                    class="text-grey-light dark:text-white">{{ userData.email }}</span>
             </div>
             <div class="mb-3">
-                <strong class="text-blue-light dark:text-blue-dark">Account Created: </strong> <span class="text-grey-light dark:text-white">{{ formatDate(userData.created_at) }}</span>
+                <strong class="text-blue-light dark:text-blue-dark">Account Created: </strong> <span
+                    class="text-grey-light dark:text-white">{{ diffForHuman(userData.created_at) }}</span> | <span
+                    class="text-grey2-light dark:text-grey2-dark">{{ userData.created_at }}</span>
             </div>
             <div class="mb-3">
-                <strong class="text-blue-light dark:text-blue-dark">Email Verified: </strong> <span v-if="userData.email_verified" class="text-green-600 dark:text-green-400">Verified</span>
+                <strong class="text-blue-light dark:text-blue-dark">Email Verified: </strong> <span
+                    v-if="userData.email_verified" class="text-green-600 dark:text-green-400">Verified</span>
                 <span v-else class="text-red-light dark:text-red-dark">Not Verified</span>
             </div>
         </div>
 
         <!-- Edit Profile Button -->
         <div v-if="userData" class="mt-6">
-            <button @click="editProfile" class="bg-blue-light hover:bg-blue-dark text-white font-bold py-2 px-4 rounded">
+            <button @click="editProfile"
+                class="bg-blue-light hover:bg-blue-dark text-white font-bold py-2 px-4 rounded">
                 Edit Profile
             </button>
         </div>
@@ -36,7 +42,10 @@
                     <div class="flex justify-between">
                         <div>
                             <strong>{{ transaction.type }}</strong>
-                            <p class="text-gray-600 dark:text-gray-400">{{ formatDate(transaction.date) }}</p>
+                            <span class="text-grey-light dark:text-white">{{
+                                diffForHuman(formatDate(transaction.created_at)) }}</span> | <span
+                                class="text-grey2-light dark:text-grey2-dark">{{ formatDate(transaction.created_at) }}
+                            </span>
                         </div>
                         <p class="font-bold" :class="transaction.amount > 0 ? 'text-green-600' : 'text-red-600'">
                             {{ transaction.amount > 0 ? '+' : '' }}{{ transaction.currency }} {{
@@ -77,6 +86,7 @@
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import axios from '../axios';
+import { diffForHuman, formatDate } from '@/dateUtils';
 
 // Refs
 const userData = ref(null);
@@ -88,23 +98,18 @@ onMounted(async () => {
         // Make API call to fetch user data
         const response = await axios.get('/users/me', { withCredentials: true });
         userData.value = response.data;
+        userData.value.created_at = formatDate(userData.value.created_at)
 
-        // // Fetch user transactions
-        // const transactionsResponse = await axios.get(`/transactions/user/${userData.value.id}`);
-        // transactions.value = transactionsResponse.data;
+        // Fetch user transactions
+        const transactionsResponse = await axios.get(`/transactions/user/${userData.value.id}`, { withCredentials: true });
+        transactions.value = transactionsResponse.data;
     } catch (error) {
         console.error('Error fetching user data or transactions:', error);
         if (error.response && error.response.status === 401) {
-            router.push('/users/login'); // Redirect to login if not authorized
+            router.push('/users/login');
         }
     }
 });
-
-// Function to format date
-const formatDate = (dateString) => {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    return new Date(dateString).toLocaleDateString(undefined, options);
-};
 
 // Placeholder functions for actions
 const editProfile = () => {
